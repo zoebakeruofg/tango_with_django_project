ab25a52046d61f6db4e688602eb127aaaa47db57
Zoe Baker <2975768b@student.gla.ac.uk>
2026-01-23 16:06:13 +0000

    3 completed passes tests.

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango may or may not say \"hey there\", depending on whether or not you are his partner")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
