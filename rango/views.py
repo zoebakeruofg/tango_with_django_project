275e9c7d0b7bcab6c53103d60387769aff4e5f3c
Zoe Baker <2975768b@student.gla.ac.uk>
2026-01-29 10:35:56 +0000

    chapter 4 complete!

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,5 @@ def index(request):
     context_dict = {"boldmessage":"Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, "rango/index.html", context=context_dict)
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {"boldmessage":"This tutorial has been put together by the zog monster. HÖUH"}
+    return render(request, "rango/about.html", context=context_dict)
