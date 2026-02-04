eb1dc467adf7325729965ff67d893aef95c36bc3
Zoe Baker <2975768b@student.gla.ac.uk>
2026-02-04 11:06:52 +0000

    7.3.2 on: https://www.tangowithdjango.com/book17/chapters/models_templates.html (since moodle is down atm)

--- rango/admin.py
+++ rango/admin.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
 class PageAdmin(admin.ModelAdmin):
     fields = ["category", "title", "url"]
     list_display = ("title", "category", "url")
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
