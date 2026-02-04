83911e6ef9b8d4d5cc1ce6b7e97f01fdef63c3e9
Zoe Baker <2975768b@student.gla.ac.uk>
2026-02-04 14:44:32 +0000

    page 104 of TWD book complete

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug = category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict["pages"] = pages
+        context_dict["category"] = category
+    except Category.DoesNotExist:
+        context_dict["category"] = None
+        context_dict["pages"] = None
+    return render(request, "rango/category.html", context=context_dict)
 
 
 def index(request):
     category_list = Category.objects.order_by("-likes")[:5]
     context_dict = {"categories":category_list}
+    context_dict["boldmessage"] = "Crunchy, creamy, cookie, candy, cupcake!"
     return render(request, "rango/index.html", context=context_dict)
+
 def about(request):
     context_dict = {"boldmessage":"This tutorial has been put together by the zog monster. HÖUH"}
     return render(request, "rango/about.html", context=context_dict)
